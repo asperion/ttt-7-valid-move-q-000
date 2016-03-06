903b85f3380c212d750769dc5a0ac4c431e711a9
asperion <brownster999@yahoo.com>
2016-03-06 22:03:48 +0000

    nitrous-backup at Sun Mar  6 22:03:48 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
 # code your #valid_move? method here
-
+def valid_move?(board, position)
+  test = /^[xo]$/i
+  if board[position] =~ test
+    return true
+  else
+    return nil
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def vaild_move?(board, position, current_user)
+  board[position.to_i - 1] = current_user
+end
